fc4694a4338b6e0cc58d378786047802ffa17112
Atsushi <atsushi@moongift.jp>
2021-09-08 13:51:55 +0900

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Atsushi on 2021/09/08.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
